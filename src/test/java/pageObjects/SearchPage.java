3e21b2435892633f176202bade27d0ca30faab59
Adityasingh1316 <2416adityasingh@gmail.com>
2025-07-09 05:26:53 +0530

    "Modified the Add to Cart Functionality "

--- src/test/java/pageObjects/SearchPage.java
+++ src/test/java/pageObjects/SearchPage.java
@@ -3,13 +3,17 @@ package pageObjects;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class SearchPage extends BasePage {
     public SearchPage(WebDriver driver){
         super(driver);
     }
 
-    @FindBy(xpath = "//input[@name=\"search\"]")
+    @FindBy(xpath = "//input[@id='input-search']")
     WebElement SearchBar;
 
     @FindBy(xpath = "//select[@class=\"form-control\"]")
@@ -20,4 +24,23 @@ public class SearchPage extends BasePage {
 
     @FindBy(xpath = "//input[@id=\"button-search\"]")
     WebElement Button;
+
+    public void setSearchBar(String search){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOf(SearchBar));
+        SearchBar.clear();
+        SearchBar.sendKeys(search);
+    }
+
+    public void setCatagories(){
+        Catagories.click();
+    }
+
+    public void setCheckBox(){
+        CheckBox.click();
+    }
+
+    public void setButton(){
+        Button.click();
+    }
 }
--- src/test/java/testCases/TC003_AddToCart.java
+++ src/test/java/testCases/TC003_AddToCart.java
@@ -1,7 +1,10 @@
 package testCases;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 import pageObjects.HomePage;
+import pageObjects.SearchPage;
 import testBase.BaseClass;
 
 public class TC003_AddToCart extends BaseClass {
@@ -10,13 +13,27 @@ public class TC003_AddToCart extends BaseClass {
     public void Verify_Add_To_cart() {
         logger.info("** Starting TC003_AddToCart **");
 
-        try{
+        try {
             HomePage hp = new HomePage(driver);
             hp.setSearch();
             logger.info("Clicked on the Search Button");
 
+            SearchPage s = new SearchPage(driver);
 
+            s.setSearchBar("iphone");
+            logger.info("Entered 'iphone' in the search bar");
 
+            s.setCheckBox();
+            logger.info("Checked the 'Search in product descriptions' checkbox");
+
+            s.setButton();
+            logger.info("Clicked the Search button");
+
+            WebElement iphone = driver.findElement(By.xpath("//button[@onclick=\"cart.add('40', '1');\"]"));
+            iphone.click();
+
+            WebElement ipod_Nano = driver.findElement(By.xpath("//button[@onclick=\"cart.add('36', '1');\"]"));
+            ipod_Nano.click();
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
